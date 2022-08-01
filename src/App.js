b5c6eed9d447ac40ae8ba7c0780ddd1a36a3f399
Chappo050 <matthewchaplin60@gmail.com>
2022-08-01 20:37:49 +1000

    async score

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import Cards from "./components/cards";
-import Score from "./components/scoring";
 
 function App() {
   const [score, setScore] = useState(0);
   return (
     <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <Score currentScore={0} bestScore={0} />
+   
       <Cards/>
       <div className="absolute bottom-30 text-black text-center text-3xl align-text-bottom">
         <i>
--- src/components/cards.js
+++ src/components/cards.js
@@ -9,62 +9,95 @@ import {
   GiFox,
 } from "react-icons/gi";
 
+
 function Cards() {
   const animalArray = [
-    <SiFoodpanda size='auto'/>,
-    <FaCat size='auto'/>,
-    <FaDog size='auto'/>,
-    <GiRabbit size='auto'/>,
-    <FaFish size='auto'/>,
-    <GiHummingbird size='auto'/>,
-    <GiDuck size='auto'/>,
-    <GiElephant size='auto'/>,
-    <GiFox size='auto'/>,
+    <SiFoodpanda size="auto" id="0"/>,
+    <FaCat size="auto" id="1"/>,
+    <FaDog size="auto"  id="2"/>,
+    <GiRabbit size="auto" id="3" />,
+    <FaFish size="auto" id="4" />,
+    <GiHummingbird size="auto"  id="5"/>,
+    <GiDuck size="auto" id="6" />,
+    <GiElephant size="auto" id="7"/>,
+    <GiFox size="auto"id="8" />,
   ];
 
-  let numberArray = [0,1,2,3,4,5,6,7,8];
+  let numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-  const [tempAnimal, setTempAnimal] = useState([]);
   const [clickedAnimal, setClickedAnimal] = useState([]);
 
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  
 
+  const ClickCard = (e) => {
 
-
-
-  return (
-    <div className="p-8 bg-gray-500 ">
-      <div className="grid grid-cols-3 gap-10 pt-20 lg:p-30">
+    
+    if (!clickedAnimal.includes(e.currentTarget.id)) {
         
-      {ShuffleArray(numberArray).map((item, i)=>{
-        return <DisplaySingleCard key={i} animals={animalArray} number={numberArray[item]}/>
-      })}
+        setScore(score + 1);
+        setClickedAnimal(clickedAnimal.concat(e.currentTarget.id))
+
+    } else{
+        console.log("FAIL");
+        if (score > bestScore) {
+            setBestScore(score)
+        }
+        setClickedAnimal([]);
+        //do shit
+    }
+
+    if(score == 9 )
+    {
+        console.log("YOU WIN")
+    }
+  };
+
+  return (  
+    <div>
+      <div className="absolute top-0 right-10 h-16 w-25 mt-10 text-xl">
+        <i>Current Score: {score} </i>
+        <br />
+        <i>Best Score: {bestScore} </i>
+      </div>
+      <div className="p-8 bg-gray-500 ">
+        <div className="grid grid-cols-3 gap-10 pt-20 lg:p-30">
+          {ShuffleArray(numberArray).map((item, i) => {
+            return (
+              <DisplaySingleCard
+                key={i}
+                animals={animalArray}
+                number={numberArray[item]}
+                click={ClickCard}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 }
 
-
-const DisplaySingleCard = ({animals, number}) => (
-
-  <div
+const DisplaySingleCard = ({ animals, number, click, }) => (
+  <div id={number}
+    onClick={click}
     className="p-4 bg-gray-400 rounded-md flex i
   tems-center justify-center w-36 lg:w-40 hover:bg-gray-600"
   >
-{animals[number]}
+    {animals[number]}
   </div>
 );
 
-
-const ShuffleArray = ( array ) =>  {
-    let i = array.length - 1;
-    for (; i > 0; i--) {
-      const j = Math.floor(Math .random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  };
-
+const ShuffleArray = (array) => {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
 
 export default Cards;
